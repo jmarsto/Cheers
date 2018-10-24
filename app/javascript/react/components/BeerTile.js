363af260fce138c10cfc8b022d793f300cda410b
Davis Ma <davishma@gmail.com>
2018-10-24 16:16:59 -0400

    redo seed-data

--- app/javascript/react/components/BeerTile.js
+++ app/javascript/react/components/BeerTile.js
@@ -4,20 +4,12 @@ import { Link } from 'react-router';
 const BeerTile = props => {
 
   return(
-<<<<<<< HEAD
-    <div className="large-2 medium-2 small-2 column beerTile">
-      <h3><Link to={`/beers/${props.id}`}>{props.name}</Link></h3>
+    <div className="large-3 medium-6 small-12 column beerTile row">
+      <h3 id = "beerTile"><Link to={`/beers/${props.id}`}>{props.name}</Link></h3>
       {props.style}<br/>
-      {props.abv}<br/>
+      {props.abv}
       <button className="deleteButton" onClick ={props.handleDelete} type="delete" value="Delete" />
     </div>
-=======
-      <div className="large-3 medium-6 small-12 column beerTile row">
-        <h3 id = "beerTile"><Link to={`/beers/${props.id}`}>{props.name}</Link></h3>
-        {props.style}<br/>
-        {props.abv}
-      </div>
->>>>>>> master
   )
 }
 
