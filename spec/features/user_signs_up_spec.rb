6ac9ee99ba81995032dbe88f22f53ae014fe981f
Alan Dust <adust1@gmail.com>
2018-10-25 16:14:58 -0400

    fixed tests

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -17,7 +17,7 @@ feature 'user registers', %Q{
 
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
     fill_in 'User Name', with: 'user name'
     fill_in 'First Name', with: 'first name'
     fill_in 'Last Name', with: 'last name'
