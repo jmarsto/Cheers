ede711736908a7a9353d01a82a7ec89c97d1bec2
Alan Dust <adust1@gmail.com>
2018-10-25 13:40:28 -0400

    Added controller test for deleting a beer

--- spec/features/controllers/api/v1/beers_controller_spec.rb
+++ spec/features/controllers/api/v1/beers_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::BeersController, type: :controller do
   let!(:beer_1) { Beer.create(name: "life", style: "light", ABV:"6.7", description: "yeah") }
   let!(:beer_2) { Beer.create(name: "beername", style: "dark", ABV:"10000", description: "yeah") }
-  let!(:user_1) { User.create(user_name: "Bob123", first_name: "Bob", last_name:"Jones", email: "bob@jones.com", encrypted_password: "123", age: 24) }
+  let!(:user_1) { User.create(user_name: "Bob123", first_name: "Bob", last_name:"Jones", email: "bob@jones.com", encrypted_password: "123", age: 24, role: "true") }
   let!(:review_1) { Review.create(body: "beername", rating: 10, user_id: user_1.id, beer: beer_1) }
 
   describe "GET#index" do
@@ -26,6 +26,24 @@ RSpec.describe Api::V1::BeersController, type: :controller do
       prev_count = Beer.count
       post(:create, params: {beer: beer_params})
       expect(Beer.count).to eq(prev_count + 1)
+    end
+  end
+
+  describe "DESTROY#delete" do
+    before(:each) do
+      user = user_1
+      allow(controller).to receive(:current_user).and_return(user)
+      get :index
+      returned_json = JSON.parse(response.body)
+      @beers_array = returned_json["beers"]
+      delete :destroy, params: {id: @beers_array[0]["id"]}
+      get :index
+      @deleted_returned_json = JSON.parse(response.body)
+      @new_beers_array = @deleted_returned_json["beers"]
+    end
+
+    it "deletes a beer" do
+       expect(@new_beers_array.length).to eq(@beers_array.length - 1)
     end
   end
 end
